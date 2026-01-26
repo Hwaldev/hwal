9349436bb12ae1bd47777b3eb535420014aa6274
Hwaldev <286893447+Hwaldev@users.noreply.github.com>
2026-01-26 17:49:19 +0900

    feat(program): scaffold anchor program

---
+++ programs/hwal/src/lib.rs
@@ -0,0 +1,10 @@
+//! Hwal trigger engine (scaffold).
+
+use anchor_lang::prelude::*;
+
+declare_id!("fSLsjTm9PGfbrAgosY2kYb1MnFEpn8LALo5cY5a4AkJ");
+
+#[program]
+pub mod hwal {
+    use super::*;
+}
