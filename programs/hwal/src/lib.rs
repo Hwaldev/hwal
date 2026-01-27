8368b4e717cedfc644b3a0deba013b2d803a7015
Hwaldev <286893447+Hwaldev@users.noreply.github.com>
2026-01-27 20:25:14 +0900

    feat(program): add errors enum

--- programs/hwal/src/lib.rs
+++ programs/hwal/src/lib.rs
@@ -1,6 +1,14 @@
-//! Hwal trigger engine (scaffold).
+//! Hwal trigger engine.
 
 use anchor_lang::prelude::*;
+
+pub mod constants;
+pub mod errors;
+pub mod state;
+
+pub use crate::constants::*;
+pub use crate::errors::HwalError;
+pub use crate::state::*;
 
 declare_id!("fSLsjTm9PGfbrAgosY2kYb1MnFEpn8LALo5cY5a4AkJ");
 
