c1b9de9eb9eabe15e7aceb2f6b21a27d8ce2bb5e
Hwaldev <286893447+Hwaldev@users.noreply.github.com>
2026-01-27 15:42:00 +0900

    feat(program): scaffold anchor program

---
+++ programs/chalna/src/lib.rs
@@ -0,0 +1,10 @@
+//! Chalna trigger engine (scaffold).
+
+use anchor_lang::prelude::*;
+
+declare_id!("fSLsjTm9PGfbrAgosY2kYb1MnFEpn8LALo5cY5a4AkJ");
+
+#[program]
+pub mod chalna {
+    use super::*;
+}
